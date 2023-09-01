eacd29842ccac6fec69a9d3f266f9516ead1f079
Carudin <110985534+Bogeng203@users.noreply.github.com>
2023-09-01 16:45:25 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-// Minimal 5 baris kode
-// alert("Welcome to my final project")
+// JavaScript untuk mengganti tampilan berdasarkan menu yang dipilih
+function showPage(pageId) {
+    // Sembunyikan semua bagian
+    document.querySelectorAll('section').forEach(section => {
+        section.style.display = 'none';
+    });
+
+    // Tampilkan bagian yang dipilih
+    document.getElementById(pageId).style.display = 'block';
+}
+
+// Tampilkan halaman "About" secara default
+showPage('about');
